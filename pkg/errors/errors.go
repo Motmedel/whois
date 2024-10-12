6baf31914b50c07cbd2b77acebbc4efbc0f21ecc
vph <vph.persson@gmail.com>
2024-10-12 21:17:35 +0200

    Copy parser errors.

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -1,9 +1,21 @@
 package errors
 
-import "errors"
+import (
+	"errors"
+	whoisparser "github.com/likexian/whois-parser"
+)
 
 var (
 	ErrNilClient   = errors.New("nil whois client")
 	ErrEmptyServer = errors.New("empty server")
 	ErrUnsetPort   = errors.New("unset port")
+)
+
+var (
+	ErrNotFoundDomain    = whoisparser.ErrNotFoundDomain
+	ErrReservedDomain    = whoisparser.ErrReservedDomain
+	ErrPremiumDomain     = whoisparser.ErrPremiumDomain
+	ErrBlockedDomain     = whoisparser.ErrBlockedDomain
+	ErrDomainDataInvalid = whoisparser.ErrDomainDataInvalid
+	ErrDomainLimitExceed = whoisparser.ErrDomainLimitExceed
 )
